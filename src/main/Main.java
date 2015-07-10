675765e18a469bccefafaf9b9999a169c9ed5dca
Morgan Xu <morgan.xu8@gmail.com>
2015-07-10 14:54:20 +1000

    20150710-Fix Conflict

--- src/main/Main.java
+++ src/main/Main.java
@@ -86,28 +86,6 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
-   
-<<<<<<< HEAD
-=======
-     public void showDashOverview() {
-        try {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(Main.class.getResource("view/Dashboard.fxml"));
-            AnchorPane dashboard = (AnchorPane) loader.load();
-
-            // Set person overview into the center of root layout.
-            rootLayout.setCenter(dashboard);
-            
-             // Give the controller access to the main app.
-        DashboardController controller = loader.getController();
-        controller.setMainApp(this);
-        
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
->>>>>>> origin/master
     
     /**
      * @param args the command line arguments
